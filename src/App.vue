8bd99ea310badfa8119baed5b4e64b7ad10d556a
Jen Merritt <jenjomez@gmail.com>
2020-01-19 14:19:30 +0000

    files setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Earthquake Tracker</h1>
+    <large-earthquake-list v-bind:largeEarthquakes="largeEarthquakes" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {eventBus} from './main.js';
+import LargeEarthquakeList from './components/LargeEarthquakeList.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    "large-earthquake-list": LargeEarthquakeList
+  },
+  data(){
+    return {
+      largeEarthquakes: []
+    };
+  },
+  mounted() {
+    fetch('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2000-01-01&endtime=2019-12-31&minmagnitude=8&orderby=magnitude')
+    .then(response => response.json())
+    .then(earthquakeData => this.largeEarthquakes = earthquakeData.features)
+    .then(banana => console.log(this.largeEarthquakes))
   }
 }
 </script>
@@ -21,8 +33,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+#app h1{
+  text-align:center;
 }
 </style>
---
+++ src/components/LargeEarthquakeList.vue
@@ -0,0 +1,83 @@
+<template lang="html">
+  <div>
+    <h3>Earthquakes from 2000 - 2020 with magnitude over 8.0</h3>
+    <!-- <ul>
+      <li v-for="earthquake in largeEarthquakes">
+        <dl>
+          <dt>Location:</dt>
+          <dd>{{ earthquake.properties.place }}</dd>
+          <dt>Magnitude:</dt>
+          <dd>{{ earthquake.properties.mag }}</dd>
+          <dt>MMI:</dt>
+          <dd>{{ earthquake.properties.mmi }}</dd>
+          <dt>Date:</dt>
+          <dd>{{ new Date(earthquake.properties.time) }}</dd>
+        </dl>
+        <hr/>
+      </li>
+    </ul> -->
+    <thead>
+      <tr>
+        <td>Location</td>
+        <td>Magnitude</td>
+        <td>MMI</td>
+        <td>Date</td>
+      </tr>
+    </thead>
+    <tr v-for="earthquake in largeEarthquakes">
+      <td>{{ earthquake.properties.place }}</td>
+      <td>{{ earthquake.properties.mag }}</td>
+      <td>{{ earthquake.properties.mmi }}</td>
+      <!-- <td>{{ new Date(earthquake.properties.time) }}</td> -->
+      <td>{{ createDate(earthquake) }}</td>
+    </tr>
+
+  </div>
+</template>
+
+<script>
+import {eventBus} from '../main.js';
+
+export default {
+  name: 'large-earthquake-list',
+  props: ['largeEarthquakes'],
+  methods: {
+    createDate(earthquake) {
+      const aDate = new Date(earthquake.properties.time);
+      const string = aDate.toDateString();
+      return string;
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+
+dd{
+  text-align:left;
+  margin: 0;
+  padding:0;
+}
+dt{
+  text-align:left;
+  font-weight:bold;
+}
+
+td{
+  padding: 20px;
+  border-bottom: 1px solid #000;
+}
+
+tr{
+  padding: 20px;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+thead{
+  font-weight: bold;
+}
+</style>
